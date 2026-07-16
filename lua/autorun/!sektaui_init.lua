2133a60736511d23198f8f597a045d2c87966fd5
sekta2 <akovlevmihail535@gmail.com>
2026-07-16 09:34:41 +0400

    docs: add init section

--- lua/autorun/!sektaui_init.lua
+++ lua/autorun/!sektaui_init.lua
@@ -1,5 +1,9 @@
 ---@class SektaUI
 _G.SektaUI = {}
 
+--[[-------------------------------------
+    Init
+--]]-------------------------------------
+
 if SERVER then AddCSLuaFile("sektaui/init.lua") end
 include("sektaui/init.lua")
