0dad5d3a46c8ea7e7a3828331a08cff83fdac843
sekta2 <akovlevmihail535@gmail.com>
2026-07-16 15:44:42 +0400

    feat: edit imports

--- lua/sektaui/init.lua
+++ lua/sektaui/init.lua
@@ -1,7 +1,27 @@
+--[[-------------------------------------
+    Modules
+--]]-------------------------------------
+
 if SERVER then
+    AddCSLuaFile("patch.lua")
     AddCSLuaFile("signal.lua")
     AddCSLuaFile("style.lua")
 else
+    include("patch.lua")
     include("signal.lua")
     include("style.lua")
 end
+
+--[[-------------------------------------
+    Elements
+--]]-------------------------------------
+
+local function load_element(name)
+    if SERVER then
+        AddCSLuaFile("elements/" .. name .. ".lua")
+    else
+        include("elements/" .. name .. ".lua")
+    end
+end
+
+load_element("base")
