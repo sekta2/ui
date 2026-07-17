883ed2dd79481e142ff1c388f1b4f0ec9c2ff682
sekta2 <akovlevmihail535@gmail.com>
2026-07-17 21:01:10 +0400

    feat: default themes

---
+++ lua/sektaui/default.lua
@@ -0,0 +1,31 @@
+--[[-------------------------------------
+    ConVar
+--]]-------------------------------------
+
+local DefaultConvar = CreateClientConVar("sektaui_theme", "derma", true)
+
+--[[-------------------------------------
+    Main
+--]]-------------------------------------
+
+local Default = {
+    Themes = {}
+}
+
+Default.Themes.Godot = SektaUI.Theme:new()
+Default.Themes.Derma = SektaUI.Theme:new()
+
+function Default:GetTheme()
+    local var = DefaultConvar:GetString()
+
+    if var == "derma" then return Default.Themes.Derma end
+    if var == "godot" then return Default.Themes.Godot end
+
+    return Default.Themes.Derma
+end
+
+--[[-------------------------------------
+    Exports
+--]]-------------------------------------
+
+SektaUI.Default = Default
