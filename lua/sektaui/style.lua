bdb581faa3a2b6b1b1fde2906d01f01c4f91126a
sekta2 <akovlevmihail535@gmail.com>
2026-07-17 21:01:30 +0400

    feat: stylebox & styleboxflat

--- lua/sektaui/style.lua
+++ lua/sektaui/style.lua
@@ -1,3 +1,5 @@
+---@diagnostic disable assign-type-mismatch
+
 --[[-------------------------------------
     Theme
 --]]-------------------------------------
@@ -51,6 +53,68 @@ function Theme:GetElementParam(name, key)
 end
 
 --[[-------------------------------------
+    StyleBox
+--]]-------------------------------------
+
+---@class SUI_StyleBox: ClassicObject
+---@field margin_left number
+---@field margin_top number
+---@field margin_right number
+---@field margin_bottom number
+---@overload fun(): SUI_StyleBox
+local StyleBox = SektaUI.Classic:extend()
+
+function StyleBox:new()
+    self.margin_left = 0
+    self.margin_top = 0
+    self.margin_right = 0
+    self.margin_bottom = 0
+end
+
+function StyleBox:Draw()
+end
+
+---@param x number
+---@param y number
+---@param w number
+---@param h number
+---@return number, number, number, number
+function StyleBox:GetContentRect(x, y, w, h)
+    return x + self.margin_left,
+        y + self.margin_top,
+        w - self.margin_left - self.margin_right,
+        h - self.margin_top - self.margin_bottom
+end
+
+--[[-------------------------------------
+    StyleBoxFlat
+--]]-------------------------------------
+
+---@class SUI_StyleBoxFlat: SUI_StyleBox
+---@field bg_color Color
+---@field border_color Color?
+---@field border_width number
+---@field corner_radius number
+---@overload fun(): SUI_StyleBoxFlat
+local StyleBoxFlat = StyleBox:extend()
+StyleBoxFlat:implement(StyleBox)
+
+function StyleBoxFlat:new()
+    self.bg_color = color_white
+    self.border_color = nil
+    self.border_width = 0
+    self.corner_radius = 0
+end
+
+function StyleBoxFlat:Draw(x, y, w, h)
+    SektaUI.RNDX.Draw(self.corner_radius, x, y, w, h, self.bg_color)
+
+    if self.border_color and self.border_width > 0 then
+        SektaUI.RNDX.DrawOutlined(self.corner_radius, x, y, w, h, self.border_color, self.border_width)
+    end
+end
+
+--[[-------------------------------------
     Exports
 --]]-------------------------------------
 
@@ -59,3 +123,6 @@ SektaUI.Theme = setmetatable(Theme, {
         return self:new(...)
     end
 })
+
+SektaUI.StyleBox = StyleBox
+SektaUI.StyleBoxFlat = StyleBoxFlat
