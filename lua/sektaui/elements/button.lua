e7f97ae053e9c6ac9bc1c36b55e48150ba55a9c3
sekta2 <akovlevmihail535@gmail.com>
2026-07-19 23:42:17 +0400

    feat(SUI_Button): add text draw & font colors

--- lua/sektaui/elements/button.lua
+++ lua/sektaui/elements/button.lua
@@ -3,11 +3,24 @@ local PANEL = {
     SUI_Class = "SUI_Button",
 }
 
+---@param x number
+---@param y number
+---@param w number
+---@param h number
+---@private
+function PANEL:DrawContent(x, y, w, h)
+    draw.SimpleText("Button", "DermaDefault", x + w / 2, y + h / 2, self:GetThemeParam(self.pressed and "font_pressed_color" or self:IsHovered() and "font_hover_color" or "font_normal_color"), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+end
+
 --[[-------------------------------------
     Style
 --]]-------------------------------------
 
 local DermaStyle = SektaUI.Default.Themes.Derma:AddElement("SUI_Button")
+
+DermaStyle.font_normal_color = GWEN.TextureColor(4 + 8 * 2, 508)
+DermaStyle.font_hover_color = GWEN.TextureColor( 4 + 8 * 3, 508 )
+DermaStyle.font_pressed_color = GWEN.TextureColor( 4 + 8 * 2, 500 )
 
 local btn_normal = SektaUI.StyleBoxTexture()
 btn_normal.material = SektaUI.Default.GWENGMod
@@ -19,14 +32,23 @@ btn_normal.mode = "border"
 
 local btn_hover = SektaUI.StyleBoxTexture()
 btn_hover.material = SektaUI.Default.GWENGMod
-btn_hover.tex_x, btn_normal.tex_y = 480, 0
-btn_hover.tex_w, btn_normal.tex_h = 31, 31
-btn_hover.margin_left, btn_normal.margin_top = 8, 8
-btn_hover.margin_right, btn_normal.margin_bottom = 8, 8
+btn_hover.tex_x, btn_hover.tex_y = 480, 32
+btn_hover.tex_w, btn_hover.tex_h = 31, 31
+btn_hover.margin_left, btn_hover.margin_top = 8, 8
+btn_hover.margin_right, btn_hover.margin_bottom = 8, 8
 btn_hover.mode = "border"
+
+local btn_pressed = SektaUI.StyleBoxTexture()
+btn_pressed.material = SektaUI.Default.GWENGMod
+btn_pressed.tex_x, btn_pressed.tex_y = 480, 96
+btn_pressed.tex_w, btn_pressed.tex_h = 31, 31
+btn_pressed.margin_left, btn_pressed.margin_top = 8, 8
+btn_pressed.margin_right, btn_pressed.margin_bottom = 8, 8
+btn_pressed.mode = "border"
 
 DermaStyle.style_normal = btn_normal
 DermaStyle.style_hover = btn_hover
+DermaStyle.style_pressed = btn_pressed
 
 --[[-------------------------------------
     Register
