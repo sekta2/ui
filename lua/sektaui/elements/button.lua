d405f06a26901db6ff8f0199635dc315b0d11559
sekta2 <akovlevmihail535@gmail.com>
2026-07-19 16:56:49 +0400

    feat: button derma style

--- lua/sektaui/elements/button.lua
+++ lua/sektaui/elements/button.lua
@@ -2,6 +2,22 @@
 local PANEL = {}
 
 --[[-------------------------------------
+    Style
+--]]-------------------------------------
+
+local DermaStyle = SektaUI.Default.Themes.Derma:AddElement("SUI_Button")
+
+local btn_normal = SektaUI.StyleBoxTexture()
+btn_normal.material = SektaUI.Default.GWENGMod
+btn_normal.tex_x, btn_normal.tex_y = 480, 0
+btn_normal.tex_w, btn_normal.tex_h = 31, 31
+btn_normal.margin_left, btn_normal.margin_top = 8, 8
+btn_normal.margin_right, btn_normal.margin_bottom = 8, 8
+btn_normal.mode = "border"
+
+DermaStyle.style_normal = btn_normal
+
+--[[-------------------------------------
     Register
 --]]-------------------------------------
 
