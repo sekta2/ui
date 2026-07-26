7dcec1b2147b0c70a27e63cc76071f0e4ef9672c
sekta2 <akovlevmihail535@gmail.com>
2026-07-26 23:18:18 +0400

    feat(init): edit imports

--- lua/sektaui/init.lua
+++ lua/sektaui/init.lua
@@ -3,6 +3,8 @@
 --]]-------------------------------------
 
 if SERVER then
+    AddCSLuaFile("vector.lua")
+    AddCSLuaFile("anchor.lua")
     AddCSLuaFile("classic.lua")
     AddCSLuaFile("rndx.lua")
     AddCSLuaFile("patch.lua")
@@ -11,6 +13,8 @@ if SERVER then
     AddCSLuaFile("font.lua")
     AddCSLuaFile("default.lua")
 else
+    include("vector.lua")
+    include("anchor.lua")
     include("classic.lua")
     include("rndx.lua")
     include("patch.lua")
@@ -38,3 +42,20 @@ load_element("basebutton")
 load_element("button")
 load_element("label")
 load_element("texture")
+load_element("center_container")
+
+if SERVER then return end
+
+concommand.Add("sui_test_center_container", function(ply, cmd, args, argStr)
+    if IsValid(gui_test_root) then
+        gui_test_root:Remove()
+    end
+
+    gui_test_root = vgui.Create("SUI_Control")
+    gui_test_root:SetSize(1680, 1050)
+
+    local button = vgui.Create("SUI_Button", gui_test_root)
+    button.custom_minimum_size = SektaUI.Vector2(100, 50)
+
+    button.anchor:PresetCenterLeft(button:GetSize())
+end)
