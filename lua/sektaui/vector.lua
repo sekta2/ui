2c5b69dc7b11673fb232e38413337e62e077da9d
sekta2 <akovlevmihail535@gmail.com>
2026-07-26 13:14:42 +0400

    docs(vector2): add `---@private` to meta-methods

--- lua/sektaui/vector.lua
+++ lua/sektaui/vector.lua
@@ -1,9 +1,15 @@
+---@diagnostic disable param-type-mismatch
+
 ---@class Vector2
 ---@field x number
 ---@field y number
+---@overload fun(x: number, y: number): Vector2
 local Vector2 = {}
 Vector2.__index = Vector2
 
+---@param x number
+---@param y number
+---@return Vector2
 function Vector2:new(x, y)
     x = x or 0
     y = y or 0
@@ -16,6 +22,7 @@ end
 
 ---@param self Vector2
 ---@param other any
+---@private
 Vector2.__add = function(self, other)
     if type(other) == "number" then
         return Vector2:new(self.x + other, self.y + other)
@@ -26,6 +33,7 @@ end
 
 ---@param self Vector2
 ---@param other any
+---@private
 Vector2.__sub = function(self, other)
     if type(other) == "number" then
         return Vector2:new(self.x - other, self.y - other)
@@ -36,6 +44,7 @@ end
 
 ---@param self Vector2
 ---@param other any
+---@private
 Vector2.__mul = function(self, other)
     if type(other) == "number" then
         return Vector2:new(self.x * other, self.y * other)
@@ -46,6 +55,7 @@ end
 
 ---@param self Vector2
 ---@param other any
+---@private
 Vector2.__div = function(self, other)
     if type(other) == "number" then
         return Vector2:new(self.x / other, self.y / other)
@@ -55,23 +65,27 @@ Vector2.__div = function(self, other)
 end
 
 ---@param self Vector2
+---@private
 Vector2.__unm = function(self)
     return Vector2:new(-self.x, -self.y)
 end
 
 ---@param self Vector2
 ---@param other Vector2
+---@private
 Vector2.__eq = function(self, other)
     return self.x == other.x and self.y == other.y
 end
 
 ---@param self Vector2
+---@private
 Vector2.__tostring = function(self)
     return string.format("Vector2(%s, %s)", self.x, self.y)
 end
 
 ---@param self Vector2
 ---@param other any
+---@private
 Vector2.__concat = function(self, other)
     return tostring(self) .. tostring(other)
 end
